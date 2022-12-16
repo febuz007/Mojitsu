d1f487a14c5c62c6e481c7788ba4280179bc48fd
febuz007 <32975534+febuz007@users.noreply.github.com>
2022-12-17 01:19:49 +0700

    Create AndroidProducts.mk

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_mojito.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_mojito-user \
+    twrp_mojito-userdebug \
+    twrp_mojito-eng
